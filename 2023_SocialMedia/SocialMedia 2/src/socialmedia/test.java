99309d590c16d5cb14d1b30f1f0ac5dfbc3a286b
Cufe12345 <73361589+Cufe12345@users.noreply.github.com>
2023-02-24 13:33:04 +0000

    Created Social Media File

---
+++ 2023_SocialMedia/SocialMedia 2/src/socialmedia/test.java
@@ -0,0 +1,15 @@
+package socialmedia;
+
+public class test implements SocialMediaPlatform {
+    public int createAccount(String c, String b) {
+        return -1;
+    }
+
+    public void removeAccount(String c) {
+
+    }
+
+    public int getNumberOfAccounts() {
+        return 100;
+    }
+}
